911baaab67410e9447ef55350c2776f6d3085dcf
GitHub Action <action@github.com>
2025-02-28 11:59:19 +0000

    Template cleanup

--- src/main/kotlin/com/github/wakyzonep/demo1/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/wakyzonep/demo1/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.wakyzonep.demo1.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
